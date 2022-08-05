135e4665810b7f148e6805032598e2fef074db61
Katagiri103 <kensuke30606@gmail.com>
2022-08-06 00:04:13 +0900

    add 2page 遅くてごめんなさい。。

--- index.php
+++ index.php
@@ -23,13 +23,46 @@
                     知識とスキルの定着率を大幅に上げることに成功しています。
                 </p>
             </div>
-            <div class="what-trainer">
+           
+        </div>
+        <div class="trainer-info">
+                <img src="<?php echo esc_url(get_theme_file_uri('/images/trainer-info-image.jpeg')); ?>" alt="">
+                <p>
+                    <span>パーソナルトレーナーとは？</span><br><br>
 
+                    パーソナルトレーナーとは、トレーニング指導の<br>
+                    専門職業で、一般の方の健康増進や<br>
+                    アスリートの競技力向上などを目的に<br>
+                    マンツーマンでエクササイズ、トレーニング指導、<br>
+                    栄養指導などを行う指導者のことです。<br><br>
+
+                    トレーニングに関する知識、経験だけではなく、<br>
+                    栄養や身体の仕組みに関する知識も必要とされ、<br>
+                    自身の鍛錬も大切な要素となります。
+                </p>
+        </div>
+        <div class="gleeting">
+            <div class="gleeting-title">
+                <h1>代表挨拶</h1>
+                <h2>社会で即活躍できるパートナルトレーナーを輩出する</h2>
             </div>
+            <div class="gleeting-self">
+                <div>
+                    <p class="gleeting-self-title">ペアトレ代表</p>
+                    <p class="gleeting-self-name">田口 優喜</p>
+                    <p class="gleeting-self-spell">TAGUCHI YUUKI</p>
+                </div>
+                <img src="<?php echo esc_url(get_theme_file_uri('/images/gleeting-self-image.jpeg')); ?>" alt="">
+            </div>
+            <div class="gleeting-image">
+                <img src="<?php echo esc_url(get_theme_file_uri('/images/gleeting-image.jpeg')); ?>" alt="">
+                
+            </div>
+            
         </div>
 
 
        
     </main>
 
-<?php get_footer(); ?>
+<!-- <?php get_footer(); ?> 
