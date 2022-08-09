d7e6a4f57377295403fdf1dfcf55c98c2376f0df
Katagiri103 <kensuke30606@gmail.com>
2022-08-09 23:59:18 +0900

    ページ追加

--- index.php
+++ index.php
@@ -42,24 +42,159 @@
                 </p>
         </div>
         <div class="gleeting">
+            
             <div class="gleeting-title">
                 <h1>代表挨拶</h1>
                 <h2>社会で即活躍できるパートナルトレーナーを輩出する</h2>
             </div>
-            <div class="gleeting-self">
+            <div class="gleeting-self-body">
                 <div>
-                    <p class="gleeting-self-title">ペアトレ代表</p>
-                    <p class="gleeting-self-name">田口 優喜</p>
-                    <p class="gleeting-self-spell">TAGUCHI YUUKI</p>
+                    <div class="gleeting-self">
+                        <div>
+                            <p class="gleeting-self-title">ペアトレ代表</p>
+                            <p class="gleeting-self-name">田口 優喜</p>
+                            <p class="gleeting-self-spell">TAGUCHI YUUKI</p>
+                        </div>
+                        <img src="<?php echo esc_url(get_theme_file_uri('/images/gleeting-self-image.jpeg')); ?>" alt="">
+                    </div>
+                    <div class="gleeting-image">
+                        <img src="<?php echo esc_url(get_theme_file_uri('/images/gleeting-image.jpeg')); ?>" alt="">
+                    </div>        
+                </div>          
+                <div class="gleeting-body">
+                    <p>
+                        パーソナルトレーナー人口が伸び続けている昨今ですが、<br>
+                        一概に資格をとればトレーナーとして安泰、知識があれば<br>
+                        トレーナーとして優秀というわけではありません。<br><br>
+                        お客様にその知識をどう伝えるのか。<br>
+                        その伝え方、ワードチョイスひとつとっても<br>
+                        お客様の理解に差が出ます。<br>
+                        <span class="font-color-blue">"できる"と"教える"とでは全く身に着けるスキルが違うのです。</span><br><br>
+                        パーソナルトレーナーには需要力と供給力<br>
+                        この２つの力が求められます。<br>
+                        <span class="font-color-blue">①需要力 ＝ お客様が求めるもの、目的、悩み、<br>
+                        ニーズを性格に把握する力<br>
+                        ②供給力 ＝ その披露したファクト（事実）に対して、<br>
+                        １番の最適な解決策を提案し、指導する力</span><br>
+                        常に時代は変化しており、それに伴いお客様のニーズも<br>
+                        多様化しています。<br>
+                        そのそれぞれのニーズに対して即座に<br>
+                        適切な解決策の提案ができるトレーナーこそが、<br>
+                        「社会で活躍できるパーソナルトレーナー」です。<br><br>
+                        当スクールが他社に比べて<br>
+                        <span class="font-color-blue">①圧倒的にコストが安く<br>
+                        ②マンツーマン指導を徹底している</span>のには２つの理由があります。<br>
+                        １つ目は金額という障壁をなくすことにより<br>
+                        学ぶハードルが低くなり、トレーナー人口拡大に繋げるため。<br>
+                        ２つ目はパーソナルトレーナーという対人の適応性が<br>
+                        問われる仕事の構造を追及した結果、<br>
+                        最適解はマンツーマン指導にたどり着くと考えたためです。<br><br>
+                        本質的な教育、指導の提供こそが社会的意義になると信じて、<br>
+                        私たちは今日も真摯に業界の課題と向き合ってまいります。
+                    </p>
                 </div>
-                <img src="<?php echo esc_url(get_theme_file_uri('/images/gleeting-self-image.jpeg')); ?>" alt="">
             </div>
-            <div class="gleeting-image">
-                <img src="<?php echo esc_url(get_theme_file_uri('/images/gleeting-image.jpeg')); ?>" alt="">
-                
-            </div>
-            
         </div>
+        <div class="concept">
+            <div>
+                <h1>コンセプト</h1>
+                <h2>「マンツーマン」×「実践型」×「低価格」</h2>
+                <p>
+                    ペアトレではすぐに即戦力のトレーナーとして活躍できるように、現場研修やロールプレイングなどの<br>
+                    実践トレーニングも重視し、社会で活躍できるトレーナー育成に取り組みます。
+                </p>
+            </div>      
+            <div class="concept-body">
+                <div class="concept1">
+                    <img src="<?php echo esc_url(get_theme_file_uri('/images/concept-image1.jpeg')); ?>" alt="">
+                    <h3>マンツーマン指導</h3>
+                    <p>
+                        講師1人対多数だとどうしてもスキルの定着<br>
+                        に差が出ます。ペアトレでは1on1の指導のため<br>
+                        一人一人に合わせて授業を行います。
+                    </p>
+                </div>
+                <div class="concept2">
+                    <img src="<?php echo esc_url(get_theme_file_uri('/images/concept-image2.jpeg')); ?>" alt="">
+                    <h3>完全実践型</h3>
+                    <p>
+                        アウトプットが講義の5割。インプットした<br>
+                        知識をその場でアウトプットしながら、実践<br>
+                        で使える生きたスキルを身につけます。
+                    </p>
+                </div>                
+                <div class="concept3">
+                    <img src="<?php echo esc_url(get_theme_file_uri('/images/concept-image3.jpeg')); ?>" alt="">
+                    <h3>低価格</h3>
+                    <p>
+                        専門学校では200～300万、他社トレー<br>
+                        ナースクールでは平均50万円はする授業料を、<br>
+                        ペアトレでは広告費を極力かけない事により、<br>
+                        常識を覆す20万という価格を実現しております。
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="course-introduction">
+            <h1>コース紹介</h1>
+            <p>
+                ペアトレでは、即戦力で活躍できる<br>
+                パーソナルトレーナーになるために2つのコースを用意しています。
+            </p>
+            <div class="course-body">
+                <div class="course-cell">
+                    <div class="course1">
+                        <h2>マンツーマンコース</h2>
+                        <img src="<?php echo esc_url(get_theme_file_uri('/images/course1.jpeg')); ?>" alt="">
+                        <p>
+                            入学金<br>
+                            33,000円（税込）<br>
+                            +<br>
+                            授業料<br>
+                            220,000円（税込）
+                        </p>
+                    </div>
+                    <div class="course2">
+                        <h2>オンライン+マンツーコース</h2>
+                        <img src="<?php echo esc_url(get_theme_file_uri('/images/course2.jpeg')); ?>" alt="">
+                        <p>
+                            入学金<br>
+                            33,000円（税込）<br>
+                            +<br>
+                            授業料<br>
+                            165,000円（税込）
+                        </p>
+                    </div>
+                </div>
+                <div class="course-bottom">
+                    <h1>\ペアトレは、こんな人におすすめします！/</h1>
+                    <p>●即パーソナルトレーナーとして働きたい</p>
+                    <p>●パーソナルトレーナーになりたいが、価格が高くて通えない</p>
+                    <p>●仕事が不規則で決まった曜日に通えない</p>
+                    <p>●運動に関わる仕事がしたいが、どうしたら良いかわからない</p>
+                    <p>●独学でボディメイクをしてきたが、活きたトレーニングスキルの習得を必要と感じている</p>
+                    <p>●一対一なら良いのだけど、スクールとかそういう団体はちょっと苦手…</p>
+                    <p>●副業としてパーソナルトレーナーとして稼ぎたい</p>
+                </div>    
+            </div>
+        </div>
+        <div class="feature">
+            <h1>ペアトレの特徴</h1>
+            <p>
+                ①現役パーソナルトレーナーから直接マンツーマン指導で学べる<br>
+                ②知識、実技だけでなくそれを伝えるアウトプット術なども学べる<br>
+                ③最短2ヶ月で卒業後に即トレーナーとして仕事ができる<br>
+                ④いつでも入学可能でじっくりと好きな時間に通える<br>
+                ⑤在学中に実践に携われる<br>
+                ⑥業界最安価格<br>
+                ⑦とにかく実践で使えるハイレベルな授業と手厚いサポート
+            </p>
+        </div>
+        
+
+            
+        
+
 
 
        
