b533c223e014bd1aae8366dd0c3be0290a79ee79
Katagiri103 <kensuke30606@gmail.com>
2022-08-11 16:58:57 +0900

    footerの編集

--- footer.php
+++ footer.php
@@ -1,14 +1,17 @@
 <footer class="site-footer">
-    
-        <h1>お気軽にお問い合わせください</h1>
-        <div class="site-footer-body">
-            <div>
-                <img class="footer-logo" src="<?php echo esc_url(get_theme_file_uri('/images/footer-logo.png')); ?>" alt="<?php bloginfo('name'); ?>">
-                <h2>TEL 000-0000-0000</h2>
+    <section class="footer-container">
+        <div class="footer-main">
+            <h1>お気軽にお問い合わせください</h1>
+            <div class="site-footer-body">
+                <div>
+                    <img class="footer-logo" src="<?php echo esc_url(get_theme_file_uri('/images/footer-logo.png')); ?>" alt="<?php bloginfo('name'); ?>">
+                    <h2>TEL 000-0000-0000</h2>
+                </div>
+                <a href="<?php echo esc_url(get_permalink(get_page_by_path('customer'))); ?>"><button>申込みはこちら▷</button></a>
+                
             </div>
-            <a href="<?php echo esc_url(get_permalink(get_page_by_path('customer'))); ?>"><button>申込みはこちら▷</button></a>
-            
         </div>
+    </section>
 </footer>
 
     <!-- Script -->
